37f586ff808009746f7b560d0e12e12139f0569b
Utkarsh seth <55540613+whoisseth@users.noreply.github.com>
2021-10-06 01:06:43 +0530

    Update script.js

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -1,34 +1,91 @@
-// 'use strict'
+ 'use strict'
+ const bill = document.querySelector('#bill');
+ const numOfPeople = document.querySelector('#num-of-people');
+ const tipAmount = document.querySelector('#tipAmount');
+ const total = document.querySelector('#total');
+ const resetBtn = document.querySelector('.rset-btn');
 
-const bill = document.querySelector('#bill');
-const numOfPeople = document.querySelector('#num-of-people');
-const tipAmount = document.querySelector('#tipAmount');
-const total = document.querySelector('#total');
-const resetBtn = document.querySelector('.rset-btn');
+ const tipBtn = document.querySelectorAll('.tip-btn')
+ let storeTipValue;
 
-const tipBtn = document.querySelectorAll('.tip-btn')
-let storeTipValue;
+ //  const tipBtn = document.querySelectorAll('.tips');
 
-const tipBtn = document.querySelectorAll('.tips');
+ const tip5p = document.getElementById("5%tip");
+ const tip10p = document.getElementById("10%tip");
+ const tip15p = document.getElementById("15%tip");
+ const tip20p = document.getElementById("20%tip");
+ const tip50p = document.getElementById("50%tip");
+ // const tip5p = document.getElementById("5%tip");
 
-const tip5p = document.getElementById("5%tip");
-const tip10p = document.getElementById("10%tip");
-const tip15p = document.getElementById("15%tip");
-const tip20p = document.getElementById("20%tip");
-const tip50p = document.getElementById("50%tip");
-// const tip5p = document.getElementById("5%tip");
+ let Custmbill;
+ let storeTipAmount;
+ let storeTotal;
+
+ //  console.log(Custmbill);
+
+ function calculateTip() {
+     let i;
+     //  Tip Amount
+     storeTipAmount = (bill.value / numOfPeople.value) * storeTipValue;
+     tipAmount.innerHTML = storeTipAmount.toFixed(2)
+         //Total
+     storeTotal = bill.value / numOfPeople.value + Number(tipAmount.innerHTML)
+     total.innerHTML = storeTotal.toFixed(2);
 
 
 
-tipBtn.addEventListener('click', function(evt) {
-    var target = evt.target;
-    if (target.id === 'tip5p') {
-        console.log('5');
-    }
 
-})
+ }
+
+ for (let i = 0; i < tipBtn.length; i++) {
+     tipBtn[0].addEventListener('click', () => {
+         storeTipValue = 0.15
+         calculateTip()
+         tipBtn[0].classList.add("tip-btn-active")
 
 
-resetBtn.addEventListener('click', function() {
 
-})
+
+     });
+
+
+     tipBtn[1].addEventListener('click', () => {
+         storeTipValue = 0.10
+         calculateTip()
+         tipBtn[1].classList.add("tip-btn-active")
+
+     });
+
+
+     tipBtn[2].addEventListener('click', () => {
+         storeTipValue = 0.15
+         calculateTip()
+
+     });
+
+
+     tipBtn[3].addEventListener('click', () => {
+         storeTipValue = 0.20
+         calculateTip()
+
+     });
+
+
+     tipBtn[4].addEventListener('click', () => {
+         storeTipValue = 0.50
+         calculateTip()
+
+     });
+
+ }
+
+
+ resetBtn.addEventListener('click', () => {
+
+     bill.value = " "
+     numOfPeople.value = " "
+     tipAmount.innerHTML = "0.00 "
+     total.innerHTML = "0.00 "
+
+
+ })
